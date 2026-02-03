9a0c7653ecaa9b5a52c40c9810da9f04d52479ea
Alpha17-2 <alphaisgod1@gmail.com>
2026-02-03 17:22:57 +0530

    bump: upgrade versionCode to 6 for release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.starcoding.contest_bell"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.5.0"
     }
 
